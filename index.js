b67ee259fd6e51be52ea0b935d6621277df61673
Gabrielle 🌿 <gabrielle@gabrieleslaptop.mynetworksettings.com>
2024-04-22 09:25:02 -0400

    added content with tutor

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Runs the application using imports from lib
+// fs is a Node standard library package for reading and writing files
+const fs = require("fs");
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/shapes");
+
+inquirer
+  .prompt([
+    {
+      type: "list",
+      name: "shape",
+      message: "please select from the following shapes: ",
+      choices: ["Circle", "Triangle", "Square"],
+    },
+    // text color
+    {
+      type: "input",
+      name: "textColor",
+      message: "please input text color",
+    },
+    //shape color
+    {
+      type: "input",
+      name: "shapeColor",
+      message: "please input shape color",
+    },
+    //text
+    {
+      type: "input",
+      name: "text",
+      //limit 3 letters of text
+      message: "please input text",
+    },
+  ])
+  .then((answers) => {
+    let shape;
+    if (answers.shape === "Circle") {
+      shape = new Circle(answers.shapeColor, answers.textColor, answers.text);
+    } else if (answers.shape === "Triangle") {
+      shape = new Triangle(answers.shapeColor, answers.textColor, answers.text);
+    } else {
+        shape = new Square(answers.shapeColor, answers.textColor, answers.text);
+    }
+
+    fs.writeFile("logo.svg", shape.render(), (err, result) => {
+      if (err) throw err;
+      console.log("svg created");
+    });
+  });
